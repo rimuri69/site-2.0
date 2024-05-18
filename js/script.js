33c9caf10af346d6942ffdfac9d108727f3e8673
rimuri69 <96191382+rimuri69@users.noreply.github.com>
2024-05-19 00:10:50 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,9 @@
 function openLoginForm() {
-    document.getElementById('loginForm').style.display = 'block';
+    alert("Logins are not working right now. Please try again later.");
 }
 
 function closeLoginForm() {
     document.getElementById('loginForm').style.display = 'none';
 }
 
-function submitLogin() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "server/authenticate.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            const response = JSON.parse(xhr.responseText);
-            if (response.success) {
-                window.location.href = "https://example.com/success?key=" + response.key;  // Change this URL to the desired destination
-            } else {
-                alert("Login failed: " + response.message);
-            }
-        }
-    };
-    xhr.send("username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password));
-}
+// Remove the submitLogin function as it will no longer be used.
