30aa0ec870f42e5c3651f9c0b0cedcd283db19c6
rimuri69 <96191382+rimuri69@users.noreply.github.com>
2024-05-19 13:41:24 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 function openLoginForm() {
     document.getElementById('loginForm').style.display = 'block';
 }
@@ -36,24 +37,17 @@ function openVouches() {
 
 function closeVouches() {
     document.getElementById('vouchesWindow').style.display = 'none';
+}
 function openPurchaseWindow() {
     document.getElementById('purchaseWindow').style.display = 'block';
-    updateTotalPrice();
 }
 
 function closePurchaseWindow() {
     document.getElementById('purchaseWindow').style.display = 'none';
 }
 
-function updateTotalPrice() {
-    const numAccounts = parseInt(document.getElementById('numAccounts').value);
-    const totalPrice = numAccounts * 4; // 4 EUR per account
-    document.getElementById('totalPrice').innerText = totalPrice + ' EUR';
-}
-
 function purchaseAccounts() {
     const numAccounts = parseInt(document.getElementById('numAccounts').value);
     const totalPrice = numAccounts * 4; // 4 EUR per account
-    // Process the purchase
     alert("You have purchased " + numAccounts + " account(s) for a total of " + totalPrice + " EUR.");
 }
